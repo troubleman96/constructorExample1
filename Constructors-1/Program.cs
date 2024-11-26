073bc9b9c02a3b78bcc6f0755ad1325c50c2f9b7
troubleman96 <itslugenge96@gmail.com>
2024-11-26 10:12:42 +0300

    first

---
+++ Constructors-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Constructir
+{
+    class Car
+    {
+
+        //parameterless
+
+        public Car()
+        {
+            Console.WriteLine("this a fortuner");
+        }
+        static void Main(string[] arg)
+        {
+            new Car();
+        }
+    }
+        
+   
+}
